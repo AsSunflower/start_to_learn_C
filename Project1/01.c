d562b4dd743520fc1790a722a33120c04f7d0a5a
AsSunflower <138982057+AsSunflower@users.noreply.github.com>
2023-07-08 22:46:30 +0800

    Add files via upload

---
+++ Project1/01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>//声明引用stdio头文件，printf函数在其中
+
+
+int main()    //主函数，int是整型,一个工程里，有且仅有一个main函数
+{
+	printf("hello world\n");//库函数之一，用来在屏幕上打印内容，需声明引用且每行语句完成需要加";",生成后加\n为换行
+
+
+		return 0;//返回像0一样的整型
+}
